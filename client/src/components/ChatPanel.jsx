0367d74d72cd8adfd636bd23c8f7739f4ffd8cee
emr4hklmn <emperorain@gmail.com>
2024-03-07 17:00:51 +0300

    finished

--- client/src/components/ChatPanel.jsx
+++ client/src/components/ChatPanel.jsx
@@ -1,82 +1,77 @@
-
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 
-const ChatPanel = (props) => {
-    const {socket,username,room}=props
-    const [currentMessage,setCurrentMessage]=useState("")
-    const [chatData,setChatData]=useState([])
-  function handleCurrentMessage(e){
-    const value=e?.target?.value
-    setCurrentMessage(value)
-  }
- async function sendMessage(){
-    if(currentMessage){
-        const messageData={
-          roomNumber:room,
-          author:username,
-          message:currentMessage,
-          time :new Date(Date.now()).getHours()+":"+new Date(Date.now()).getMinutes() 
-        }
-        await socket.emit("send_message",messageData)
-        
-        // setChatData(prev=>[...prev,messageData])
-        setCurrentMessage("")
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
-    
-  }
-    useEffect(()=>{
-      socket.on("receive_message",(data)=>{
-        console.log("data new",data)
-        setChatData(prev=>[...prev,data])
-      })
-    },[socket])
-    
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
+
   return (
     <div className="chat-window">
-    <div className="chat-header">
-      <div className='live-panel'></div>
-      <p>Live Chat</p>
-    </div>
-    <div className="chat-body">
-      <ScrollToBottom className="message-container">
-        {chatData?.map(({time,author,username,message}) => {
-          return (
-            <div
-            key={time}
-              className="message"
-              id={username === author ? "you" : "other"}
-            >
-              <div className='message-panel'>
-                <div className="message-content">
-                  <p>{message}</p>
-                </div>
-                <div className="message-meta">
-                  <p id="time">{time}</p>
-                  <p id="author">{author}</p>
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "other" : "you"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </ScrollToBottom>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
     </div>
-    <div className="chat-footer">
-      <input
-        type="text"
-        value={currentMessage}
-        placeholder="Hey..."
-        onChange={(e) => {
-          handleCurrentMessage(e);
-        }}
-        onKeyPress={(event) => {
-          event.key === "Enter" && sendMessage();
-        }}
-      />
-      <button onClick={sendMessage}>&#9658;</button>
-    </div>
-  </div>
-  )
+  );
 }
 
-export default ChatPanel
+export default Chat;
